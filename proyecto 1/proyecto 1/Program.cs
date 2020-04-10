a18148d6daeda0c2263d5087e6f5b047b7ae6db9
Ivan_Orejarena <idorejarena@misena.edu.co>
2020-04-10 18:00:59 -0500

    proyecto

---
+++ proyecto 1/proyecto 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int edad = 0;
+            bool permiso = false;
+            string valor = "";
+            
+
+            Console.WriteLine(" dame la edad");
+            valor = Console.ReadLine();
+            edad = Convert.ToInt32(valor);
+
+            Console.WriteLine("tiene permiso de los padres(true/false): ");
+            valor = Console.ReadLine();
+            permiso = Convert.ToBoolean(valor);
+
+            if(edad >18|| (edad > 15 && permiso == true))
+                Console.WriteLine("es posible condu");
+
+            else
+                Console.WriteLine("no puede ");
+
+
+        }
+    }
+}
